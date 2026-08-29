b7a10174f8a13a9ff02f9cf3531b88dd1466964f
OrzMiku <miku@ecy.pink>
2026-08-30 01:19:08 +0800

    chore: 添加 neovim config link

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,13 @@ import Logo from "../Logo/Logo";
 import styles from "./App.module.css";
 
 function App() {
-  const links: Link[] = [{ text: "github", url: "https://github.com/OrzMiku" }];
+  const links: Link[] = [
+    { text: "github", url: "https://github.com/OrzMiku" },
+    {
+      text: "my neovim config",
+      url: "https://github.com/OrzMiku/neovim-config",
+    },
+  ];
   return (
     <div className={styles.app}>
       <Logo title="OrzMiku" />
